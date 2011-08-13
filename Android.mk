53387d81c099efae8f3c1a099c3b49d93dde7fe5
podxboq <podxboq@gmail.com>
2011-08-14 00:45:10 +0200

    DEL: Test module
    Change-Id: I3947aed63dba251aa05bc569ecf57e44f7fefc5c

--- Android.mk
+++ Android.mk
@@ -19,6 +19,3 @@ LOCAL_REQUIRED_MODULES := SoundRecorder
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
-
-# This finds and builds the test apk as well, so a single make does both.
-include $(call all-makefiles-under,$(LOCAL_PATH))
